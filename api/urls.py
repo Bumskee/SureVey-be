2a18fba9876a05a77eb49cbdce5794233507a44a
nathaniel-alvin <alvinparali@gmail.com>
2022-06-07 18:00:11 +0700

    fix url and add API for document details

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, re_path
 from django.conf.urls import include
 from rest_framework import routers
-from .views import UserViewSet
+from .views import UserViewSet, documentDetails
 from api.views import documentApi
 
 router = routers.DefaultRouter()
@@ -10,7 +10,6 @@ router.register('users', UserViewSet)
 
 urlpatterns = [
     # path('', include(router.urls)),
-    re_path(r'^form/(?P<id>[0-9a-f-]+)', documentApi),
-    re_path(r'^form$', documentApi),
+    re_path(r'^form/(?P<id>[0-9a-f-]+)', documentDetails),
     re_path(r'get_all_forms', documentApi)
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from api.models import Documents
+from urllib import response
 from api.serializers import DocumentSerializer
 from django.shortcuts import render
 from rest_framework import viewsets
@@ -7,6 +7,11 @@ from django.contrib.auth import get_user_model
 from .serializers import UserSerializer
 from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+from api.models import Documents
 
 User = get_user_model()
 
@@ -16,14 +21,37 @@ class UserViewSet(viewsets.ModelViewSet):
 
 @csrf_exempt
 def documentApi(request, id=0):
-    if request.method=='POST':
+    if request.method=='GET':
+        documents = Documents.objects.all()
+        documents_serializer = DocumentSerializer(documents, many=True)
+        return JsonResponse(documents_serializer.data, safe=False)
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def documentDetails(request, id):
+    if request.method == 'GET':
+        document = Documents.objects.get(DocumentID=id)
+        serializer = DocumentSerializer(document)
+        return JsonResponse(serializer.data)
+
+    elif request.method=='POST':
         document_data = JSONParser().parse(request)
         documents_serializer = DocumentSerializer(data=document_data)
         if documents_serializer.is_valid():
             documents_serializer.save()
-            return JsonResponse("Added Successfully", safe=False)
+            return Response(status=status.HTTP_201_CREATED)
         return JsonResponse("Failed to Add", safe=False)
-    elif request.method=='GET':
-        documents = Documents.objects.all()
-        documents_serializer = DocumentSerializer(documents, many=True)
-        return JsonResponse(documents_serializer.data, safe=False)
+
+    elif request.method == 'PUT':
+        document = Documents.objects.get(DocumentID=id)
+        # document_data = JSONParser().parse(request)
+        documents_serializer = DocumentSerializer(document, data=request.data)
+        if documents_serializer.is_valid():
+            documents_serializer.save()
+            return Response(documents_serializer.data)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        document = Documents.objects.get(DocumentID=id)
+        document.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
